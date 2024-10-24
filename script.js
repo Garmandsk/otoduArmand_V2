3f241aa676c1ce6edd35739ceb6776d883a107a3
Garmandsk <xmiagertiarmanda@gmail.com>
2024-10-24 18:14:48 +0700

    Pengetesan DOM Part 2

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-const navMentorOtodu = document.getElementById("navMentorOtodu");
-navMentorOtodu.innerText = "Mentor Otodu";
+/* Bisa Mengakses Text */
+document.querySelector("#navMentorOtodu").innerText = "Mentor Otodu";
+
+/* Belum Bisa Mengakses Tag (Jika Diaktifkan Elemennya Hilang) 
+document.querySelector("#konten").innerHTML = `<h1>Hallo Dunia</h1>`;
+*/
+
+/* Belum Bisa Mengakses Tag, Tapi Bisa Mengubah Warna Text (Jika Diaktifkan)
+document.querySelector("#navbar").innerHTML = `<h1 style="color:red;">Hallo Sekai</h1>`;
+*/
